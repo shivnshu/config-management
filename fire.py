8c7bd66053e6449ac15292b6246a294921a8ec64
Shivanshu Singh <shivanshusinghraj@gmail.com>
2018-06-03 12:20:33 +0530

    Add oh-my-zsh

--- fire.py
+++ fire.py
@@ -3,6 +3,7 @@ import os
 import sys
 import yaml
 import subprocess
+import shutil
 
 def readConfig(fileName):
     fd = open(fileName, "r")
@@ -21,6 +22,8 @@ def executeCommand(task, conf_dir):
 
 if __name__ == "__main__":
     print("Firing config manager..")
+    if input("Are you sure? (y/n): ") != "y":
+        sys.exit(0)
     configFileLocation = 'conf.yml'
     if len(sys.argv) == 1:
         print("Using default: conf.yml config")
@@ -40,20 +43,35 @@ if __name__ == "__main__":
     for repo in cfgObject['subrepos']:
         _location = os.path.join(conf_dir, os.path.expandvars(repo['location']))
         _origin = repo['origin']
+        if os.path.exists(_location):
+            print("Directory for " + repo['name'] + " already exists.")
+            if input("[R]eplace?: ") != "R":
+                continue
+            shutil.rmtree(_location)
         print("Cloning " + repo['name'])
         subprocess.call(["git", "clone", _origin, _location])
 
     # Softlinking directories
     for direc in cfgObject['directories']:
         _placed = os.path.join(conf_dir, os.path.expandvars(direc['placed']))
-        _location = direc['location']
+        _location = os.path.expandvars(direc['location'])
+        if os.path.exists(_location):
+            print("Directory for " + direc['name'] + " already exists.")
+            if input("[R]eplace?: ") != "R":
+                continue
+            shutil.rmtree(_location)
         print("Softlinking " + direc['name'])
         subprocess.call(["ln", "-s", _placed, _location])
 
     # Softlinking files
     for f in cfgObject['files']:
-        _placed = os.path.join(conf_dir, os.path.expandvars(f['_placed']))
-        _location = f['location']
+        _placed = os.path.join(conf_dir, os.path.expandvars(f['placed']))
+        _location = os.path.expandvars(f['location'])
+        if os.path.exists(_location):
+            print("File for " + f['name'] + " already exists.")
+            if input("[R]eplace?: ") != "R":
+                continue
+            os.remove(_location)
         print("Softlinking " + f['name'])
         subprocess.call(["ln", "-s", _placed, _location])
 
